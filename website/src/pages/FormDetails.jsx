ccc7417bdf3556bcd0304845f18aca48298b38c1
geethika1809 <mareedugeethika@gmail.com>
2023-12-22 18:47:17 +0530

    geethika user profile done

--- website/src/pages/FormDetails.jsx
+++ website/src/pages/FormDetails.jsx
@@ -1,15 +1,103 @@
-import React from 'react'
-import Navibar from '../components/Navibar'
+// ProfileForm.js
+import React, { useState } from 'react';
 
-function FormDetails() {
+
+const ProfileForm = () => {
+  const [photo, setPhoto] = useState(null);
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+  const [address, setAddress] = useState('');
+  const [editMode, setEditMode] = useState(true);
+  const [submittedData, setSubmittedData] = useState(null);
+
+  const handlePhotoChange = (e) => {
+    const file = e.target.files[0];
+    setPhoto(file);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add logic to handle form submission (e.g., send data to server)
+    const submittedInfo = { photo, name, surname, address };
+    console.log('Form submitted:', submittedInfo);
+    setSubmittedData(submittedInfo);
+    setEditMode(false); // Switch to view mode after submission
+  };
+
+  const handleEdit = () => {
+    setEditMode(true); // Switch back to edit mode
+  };
+
   return (
-    <div>
-       <Navibar />
+    <div className="profile-form-container">
+      <div className="sidebar">
+        <div className="profile-details">
+          <h2>Profile Details</h2>
+          
+        </div>
+        <div className="links">
+          <a href="/login">Logout</a>
+          <a href="#">Change Password</a>
+        </div>
+      </div>
 
-       <div></div>
-
+      {editMode ? (
+        <form onSubmit={handleSubmit} className="form-container">
+          <label>
+            <div className="upload-container">
+              <div className="example-image-container">
+                {photo ? (
+                  <img src={URL.createObjectURL(photo)} alt="Uploaded" style={{ width: '90px', height: '90px', borderRadius: '20%' }} />
+                ) : (
+                  <img src="https://www.shutterstock.com/image-vector/user-profile-icon-vector-avatar-600nw-2220431045.jpg" alt="Example" style={{ width: '100px', height: '100px', borderRadius: '50%' }} />
+                )}
+              </div>
+              Upload Photo:
+              <input type="file" onChange={handlePhotoChange} accept="image/*" />
+            </div>
+          </label>
+          <div className="input-container">
+            <label>
+              Name:
+              <input type="text" value={name} onChange={(e) => setName(e.target.value)} required />
+            </label>
+            <label>
+              Surname:
+              <input type="text" value={surname} onChange={(e) => setSurname(e.target.value)} required />
+            </label>
+          </div>
+          <div className="input-container">
+            <label>
+              Address:
+              <input type="text" value={address} onChange={(e) => setAddress(e.target.value)} required />
+            </label>
+          </div>
+          <div className="button-container">
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      ) : null}
+      
+      {!editMode && !submittedData && (
+        <div className="no-details-message">
+          <p>No details submitted yet.</p>
+        </div>
+      )}
+      {!editMode && submittedData && (
+        <div className="submitted-details">
+          <h2>Your Details</h2>
+          <p>Name: {submittedData.name}</p>
+          <p>Surname: {submittedData.surname}</p>
+          <p>Address: {submittedData.address}</p>
+          <div className="button-container">
+            <button type="button" onClick={handleEdit}>
+              Edit
+            </button>
+          </div>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default FormDetails
+export default ProfileForm;
